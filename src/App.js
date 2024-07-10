dcf2bf269f7c2d04041d310b283dbf72bc2e2f1d
Ajer <ronnebyanders@hotmail.com>
2024-07-10 13:57:34 +0200

    Gjorde routingen till de olika kategori-sidorna

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 import Header from './pages/components/header';
 import Categories from './pages/components/Categories';
 import Footer from './pages/components/footer';
+import Category from './pages/components/Category';
 
 
 function App() {
@@ -21,7 +22,14 @@ function App() {
             <Routes>
                 <Route path="/" element={<Header />}> {/*Header element, sidor som barn under */}
                     <Route index element={<Categories />} />
+                    <Route path="/Hemelektronik" element={<Category id={1} catName={'Hemelektronik'} />} />
+                    <Route path="/Resor" element={<Category id={2} catName={'Resor'} />} />
+                    <Route path="/Mat" element={<Category id={3} catName={'Mat'} />} />
+                    <Route path="/Klader" element={<Category id={4} catName={'Kläder'} />} />
+                    <Route path="/Sport" element={<Category id={5} catName={'SportPrylar'} />} />
+                    <Route path="/Matlagning" element={<Category id={6} catName={'Matlagningsprylar'} />} />
                 </Route>
+                
             </Routes>
             <Footer /> {/*Footer element */}
         </BrowserRouter>
---
+++ src/pages/components/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Category.css';
+
+export default function Category({id,catName}) {
+  return (
+    <div>
+       <h2>{catName}</h2>
+
+
+    </div>
+  )
+}
