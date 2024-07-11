053f73491eb65d239313deb688e5c24bd49857be
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-07-11 10:32:01 +0200

    Kommenterade i produktkort

--- src/pages/components/produktkort.jsx
+++ src/pages/components/produktkort.jsx
@@ -11,30 +11,6 @@ const demoProdukt = {
     "quantity": 101
 };
 
-
-/* const demoLista = [{"brand":"Dell","name":"Vostro 3520 Core i5 16GB 512GB 15.6","type":"electronics","image":"dell-vostro-3520-core-i5-16gb-512gb-156.jpg",
-    "price":5490,"original_price": 7490,"quantity": 101},
-    {"brand":"Samsung","name":"512GB 15.6","type":"something_else","image":"hjhj.jpg",
-        "price":2490,"original_price": 3490,"quantity": 101},
-        {"brand":"HP","name":"HP 3520 Core i5 16GB 512GB 15.6","type":"electronics","image":"db-156.jpg",
-            "price":5490,"original_price": 7490,"quantity": 101}
-]; */
-
-// const demo = JSON.parse('"' + demoLista[0] + '"');
-
-//const demo = JSON.parse(demoLista[0]);
-
-// demoLista.sort((a, b) => (a.type > b.type? 1 : -1));
-
-/* const demo = demoLista.filter(el=>
-    el.type=="electronics"
-);
-console.log(demo.length);
-console.log(demo[0].type,demo[1].type); */
-
-
-// console.log(demoLista[0].type,demoLista[1].type,demoLista[2].type);
-
 /*
 Produkt = {
     brand,
@@ -47,7 +23,10 @@ Produkt = {
 }
 */
 
+//Parametern "produkt" är 1st produkt (ser ut som demoProdukt)
 export default function ProduktKort({ produkt }) {
+
+    //Content = produkt - Alla värden i produkt-objektet blir content.värde
     const [ content, setContent ] = useState(demoProdukt);
 
     //Ändra w-25 när kortet har en container som sätter storleken
@@ -55,11 +34,19 @@ export default function ProduktKort({ produkt }) {
          <div className="card text-start m-2 w-25" style={{minWidth:'220px'}}>  
             <img src={require(`../../../public/data/thumbnails/${content.image}`)} alt={content.name} className="card-img-top" />
             <div className="card-body">
+
+                {/* content.brand = produkt.brand */}
                 <p className="card-text my-0">{content.brand}</p>
+
+                {/* content.name = produkt.name */}
                 <h5 className="card-title">{content.name}</h5>
                 <div className="container-fluid row px-0 py-3">
                     <div className="col-9 d-flex flex-column justify-content-between">
+
+                        {/* content.price/original_price = produkt.price/original_price */}
                         <CardPrice price={content.price} orgPrice={content.original_price} />
+
+                        {/* content.quantity = produkt.quantity */}
                         <CardQuantity amount={content.quantity} />
                     </div>
                     <div className="col-3 text-center">
@@ -71,20 +58,24 @@ export default function ProduktKort({ produkt }) {
     );
 };
 
+//Parametern "amount" är Produktens "quantity" värde
 function CardQuantity({amount}) {
 
+    //Om mängden produkter är över 100, text blir 100+
     if (amount > 100) return (
         <h6 className="card-subtitle">
             <span><i className="bi bi-box-seam-fill text-success  fs-5"></i>{` 100+ på lager`}</span>
         </h6>
     );
 
+    //Om mängden är under 1 så visas Slut på lager
     if (amount < 1) return (
         <h6 className="card-subtitle">
             <span><i className="bi bi-box-seam-fill text-danger fs-5"></i>{` Slut på lager`}</span>
         </h6>
     );
 
+    //Om värdet är 1-100 visas värdet
     return (
         <h6 className="card-subtitle">
             <span><i className="bi bi-box-seam-fill"></i>{` ${amount}st kvar`}</span>
@@ -92,14 +83,17 @@ function CardQuantity({amount}) {
     );
 };
 
+//Parametrarna "price, orgPrice" är produktens "price, original_Price" värden
 function CardPrice({price, orgPrice}) {
     
+    //Om orgPrice är satt, använd det men stryk över det
     if (orgPrice > 0) return (
         <h6 className="card-subtitle">
             <span className="text-decoration-line-through">{orgPrice}</span> {` ${price} SEK`}
         </h6>
     );
 
+    //Annars visas endast priset
     return (
         <h6 className="card-subtitle">
             {` ${price} SEK`}
