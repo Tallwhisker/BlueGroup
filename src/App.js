624300897fcdd822afbfe2c10bde040a0737c86a
KevinPC <164023995+Tallwhisker@users.noreply.github.com>
2024-07-12 09:53:25 +0200

    Fixed klader -> kläder category name

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
                     <Route path="/Hemelektronik" element={<Category id={1} catName={'Hemelektronik'} prodList={databas.electronics} />} />
                     <Route path="/Resor" element={<Category id={2}  catName={'Resor'} prodList={databas.travel} />} />
                     <Route path="/Mat" element={<Category id={3} catName={'Mat'} prodList={databas.food} />} />
-                    <Route path="/Klader" element={<Category id={4} catName={'Klader'} prodList={databas.clothing} />} />
+                    <Route path="/Klader" element={<Category id={4} catName={'Kläder'} prodList={databas.clothing} />} />
                     <Route path="/Sport" element={<Category id={5} catName={'SportPrylar'} prodList={databas.sports} />} />
                     <Route path="/Matlagning" element={<Category id={6} catName={'Matlagningsprylar'} prodList={databas.kitchenware} />} />
                 </Route>
